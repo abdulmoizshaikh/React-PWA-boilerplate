dbf34df1e5889fa652e47777f6984624366ac84e
abdulmoizshaikh <muhammadmoizshaikh@gmail.com>
2021-03-03 16:31:27 +0500

    Revert "downgrade react and react dom version and react script version from 17 to 16 in order to PWA to work on Android"
    This reverts commit fa013307ec14953974bfe0f6d2c5f0025484607c.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 // import reportWebVitals from "./reportWebVitals";
 import * as serviceWorker from "./serviceWorker";
 
@@ -14,5 +15,11 @@ ReactDOM.render(
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// Learn more about service workers: https://cra.link/PWA
+// serviceWorkerRegistration.register();
 serviceWorker.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
